367fa06d68f03c7c286cc62a4c8ac99176b90fb1
Patzxaxaxa6 <coolguigameplay@gmail.com>
2026-09-20 14:27:45 +0700

    Update StageData.hx

--- source/backend/StageData.hx
+++ source/backend/StageData.hx
@@ -1,6 +1,8 @@
 package backend;
 
-import openfl.utils.Assets;
+import openfl.utils.Assets as OpenFlAssets;
+import openfl.utils.AssetType;
+
 import haxe.Json;
 import backend.Song;
 import psychlua.ModchartSprite;
@@ -35,7 +37,8 @@ enum abstract LoadFilters(Int) from Int from UInt to Int to UInt
 	var FREEPLAY:Int = (1 << 3);
 }
 
-class StageData {
+class StageData
+{
 	public static function dummy():StageFile
 	{
 		return {
@@ -62,166 +65,459 @@ class StageData {
 	}
 
 	public static var forceNextDirectory:String = null;
-	public static function loadDirectory(SONG:SwagSong) {
-		var stage:String = '';
-		if(SONG.stage != null)
-			stage = SONG.stage;
-		else if(Song.loadedSongName != null)
-			stage = vanillaSongStage(Paths.formatToSongPath(Song.loadedSongName));
-		else
-			stage = 'stage';
 
-		var stageFile:StageFile = getStageFile(stage);
-		forceNextDirectory = (stageFile != null) ? stageFile.directory : ''; //preventing crashes
+	public static function loadDirectory(SONG:SwagSong)
+	{
+		var stage:String = '';
+
+		if (SONG.stage != null)
+		{
+			stage = SONG.stage;
+		}
+		else if (Song.loadedSongName != null)
+		{
+			stage =
+				vanillaSongStage(
+					Paths.formatToSongPath(
+						Song.loadedSongName
+					)
+				);
+		}
+		else
+		{
+			stage = 'stage';
+		}
+
+		var stageFile:StageFile =
+			getStageFile(stage);
+
+		/*
+		 * Prevent crashes when the stage file doesn't exist.
+		 */
+		forceNextDirectory =
+			(stageFile != null)
+				? stageFile.directory
+				: '';
 	}
 
-	public static function getStageFile(stage:String):StageFile {
-		try
+	/*
+	 * =========================================================
+	 * STAGE JSON
+	 * =========================================================
+	 *
+	 * Main location:
+	 *
+	 * assets/BFEXEOPT/stages/<stage>.json
+	 *
+	 * No external mods/ lookup is performed here.
+	 */
+
+	public static function getStageFile(
+		stage:String
+	):StageFile
+	{
+		if (
+			stage == null
+			|| stage.trim().length == 0
+		)
 		{
-			var path:String = Paths.getPath('stages/' + stage + '.json', TEXT, null, true);
-			#if MODS_ALLOWED
-			if(FileSystem.exists(path))
-				return cast tjson.TJSON.parse(File.getContent(path));
-			#else
-			if(Assets.exists(path))
-				return cast tjson.TJSON.parse(Assets.getText(path));
-			#end
+			stage = 'stage';
 		}
+
+		stage =
+			Paths.formatToSongPath(stage);
+
+		var path:String =
+			Paths.getGameContentPath(
+				'stages/$stage.json'
+			);
+
+		/*
+		 * Packaged APK asset.
+		 */
+		if (
+			OpenFlAssets.exists(
+				path,
+				AssetType.TEXT
+			)
+		)
+		{
+			try
+			{
+				var rawJson:String =
+					OpenFlAssets.getText(path);
+
+				if (
+					rawJson != null
+					&& rawJson.length > 0
+				)
+				{
+					return cast tjson.TJSON.parse(
+						rawJson
+					);
+				}
+			}
+			catch (e:Dynamic)
+			{
+				trace(
+					'Failed to load stage JSON: '
+					+ path
+				);
+
+				trace(e);
+			}
+		}
+
+		/*
+		 * If the requested stage doesn't exist,
+		 * return the safe default stage.
+		 */
+		trace(
+			'Stage file not found: '
+			+ path
+		);
+
 		return dummy();
 	}
 
-	public static function vanillaSongStage(songName):String
+	public static function vanillaSongStage(
+		songName
+	):String
 	{
 		switch (songName)
 		{
 			case 'spookeez' | 'south' | 'monster':
 				return 'spooky';
+
 			case 'pico' | 'blammed' | 'philly' | 'philly-nice':
 				return 'philly';
+
 			case 'milf' | 'satin-panties' | 'high':
 				return 'limo';
+
 			case 'cocoa' | 'eggnog':
 				return 'mall';
+
 			case 'winter-horrorland':
 				return 'mallEvil';
+
 			case 'senpai' | 'roses':
 				return 'school';
+
 			case 'thorns':
 				return 'schoolEvil';
+
 			case 'ugh' | 'guns' | 'stress':
 				return 'tank';
 		}
+
 		return 'stage';
 	}
 
-	public static var reservedNames:Array<String> = ['gf', 'gfGroup', 'dad', 'dadGroup', 'boyfriend', 'boyfriendGroup']; //blocks these names from being used on stage editor's name input text
-	public static function addObjectsToState(objectList:Array<Dynamic>, gf:FlxSprite, dad:FlxSprite, boyfriend:FlxSprite, ?group:Dynamic = null, ?ignoreFilters:Bool = false)
+	public static var reservedNames:Array<String> = [
+		'gf',
+		'gfGroup',
+		'dad',
+		'dadGroup',
+		'boyfriend',
+		'boyfriendGroup'
+	];
+
+	public static function addObjectsToState(
+		objectList:Array<Dynamic>,
+		gf:FlxSprite,
+		dad:FlxSprite,
+		boyfriend:FlxSprite,
+		?group:Dynamic = null,
+		?ignoreFilters:Bool = false
+	)
 	{
 		var addedObjects:Map<String, FlxSprite> = [];
+
 		for (num => data in objectList)
 		{
-			if (addedObjects.exists(data)) continue;
+			if (addedObjects.exists(data))
+				continue;
 
 			switch(data.type)
 			{
 				case 'gf', 'gfGroup':
-					if(gf != null)
+					if (gf != null)
 					{
-						gf.ID = num; 
-						if (group != null) group.add(gf);
-						addedObjects.set('gf', gf);
+						gf.ID = num;
+
+						if (group != null)
+							group.add(gf);
+
+						addedObjects.set(
+							'gf',
+							gf
+						);
 					}
+
 				case 'dad', 'dadGroup':
-					if(dad != null)
+					if (dad != null)
 					{
 						dad.ID = num;
-						if (group != null) group.add(dad);
-						addedObjects.set('dad', dad);
+
+						if (group != null)
+							group.add(dad);
+
+						addedObjects.set(
+							'dad',
+							dad
+						);
 					}
+
 				case 'boyfriend', 'boyfriendGroup':
-					if(boyfriend != null)
+					if (boyfriend != null)
 					{
 						boyfriend.ID = num;
-						if (group != null) group.add(boyfriend);
-						addedObjects.set('boyfriend', boyfriend);
+
+						if (group != null)
+							group.add(boyfriend);
+
+						addedObjects.set(
+							'boyfriend',
+							boyfriend
+						);
 					}
 
 				case 'square', 'sprite', 'animatedSprite':
-					if(!ignoreFilters && !validateVisibility(data.filters)) continue;
-
-					var spr:ModchartSprite = new ModchartSprite(data.x, data.y);
-					spr.ID = num;
-					if(data.type != 'square')
+					if (
+						!ignoreFilters
+						&& !validateVisibility(
+							data.filters
+						)
+					)
 					{
-						if(data.type == 'sprite')
-							spr.loadGraphic(Paths.image(data.image));
-						else
-							spr.frames = Paths.getAtlas(data.image);
-						
-						if(data.type == 'animatedSprite' && data.animations != null)
+						continue;
+					}
+
+					var spr:ModchartSprite =
+						new ModchartSprite(
+							data.x,
+							data.y
+						);
+
+					spr.ID = num;
+
+					if (data.type != 'square')
+					{
+						if (data.type == 'sprite')
 						{
-							var anims:Array<objects.Character.AnimArray> = cast data.animations;
+							spr.loadGraphic(
+								Paths.image(
+									data.image
+								)
+							);
+						}
+						else
+						{
+							spr.frames =
+								Paths.getAtlas(
+									data.image
+								);
+						}
+
+						if (
+							data.type == 'animatedSprite'
+							&& data.animations != null
+						)
+						{
+							var anims:Array<objects.Character.AnimArray> =
+								cast data.animations;
+
 							for (key => anim in anims)
 							{
-								if(anim.indices == null || anim.indices.length < 1)
-									spr.animation.addByPrefix(anim.anim, anim.name, anim.fps, anim.loop);
+								if (
+									anim.indices == null
+									|| anim.indices.length < 1
+								)
+								{
+									spr.animation.addByPrefix(
+										anim.anim,
+										anim.name,
+										anim.fps,
+										anim.loop
+									);
+								}
 								else
-									spr.animation.addByIndices(anim.anim, anim.name, anim.indices, '', anim.fps, anim.loop);
-	
-								if(anim.offsets != null)
-									spr.addOffset(anim.anim, anim.offsets[0], anim.offsets[1]);
-	
-								if(spr.animation.curAnim == null || data.firstAnimation == anim.anim)
-									spr.playAnim(anim.anim, true);
+								{
+									spr.animation.addByIndices(
+										anim.anim,
+										anim.name,
+										anim.indices,
+										'',
+										anim.fps,
+										anim.loop
+									);
+								}
+
+								if (anim.offsets != null)
+								{
+									spr.addOffset(
+										anim.anim,
+										anim.offsets[0],
+										anim.offsets[1]
+									);
+								}
+
+								if (
+									spr.animation.curAnim == null
+									|| data.firstAnimation == anim.anim
+								)
+								{
+									spr.playAnim(
+										anim.anim,
+										true
+									);
+								}
 							}
 						}
-						for (varName in ['antialiasing', 'flipX', 'flipY'])
+
+						for (
+							varName in [
+								'antialiasing',
+								'flipX',
+								'flipY'
+							]
+						)
 						{
-							var dat:Dynamic = Reflect.getProperty(data, varName);
-							if(dat != null) Reflect.setProperty(spr, varName, dat);
+							var dat:Dynamic =
+								Reflect.getProperty(
+									data,
+									varName
+								);
+
+							if (dat != null)
+							{
+								Reflect.setProperty(
+									spr,
+									varName,
+									dat
+								);
+							}
 						}
-						if(!ClientPrefs.data.antialiasing) spr.antialiasing = false;
+
+						if (!ClientPrefs.data.antialiasing)
+							spr.antialiasing = false;
 					}
 					else
 					{
-						spr.makeGraphic(1, 1, FlxColor.WHITE);
+						spr.makeGraphic(
+							1,
+							1,
+							FlxColor.WHITE
+						);
+
 						spr.antialiasing = false;
 					}
 
-					if(data.scale != null && (data.scale[0] != 1.0 || data.scale[1] != 1.0))
+					if (
+						data.scale != null
+						&& (
+							data.scale[0] != 1.0
+							|| data.scale[1] != 1.0
+						)
+					)
 					{
-						spr.scale.set(data.scale[0], data.scale[1]);
+						spr.scale.set(
+							data.scale[0],
+							data.scale[1]
+						);
+
 						spr.updateHitbox();
 					}
-					spr.scrollFactor.set(data.scroll[0], data.scroll[1]);
-					spr.color = CoolUtil.colorFromString(data.color);
-					
-					for (varName in ['alpha', 'angle'])
+
+					spr.scrollFactor.set(
+						data.scroll[0],
+						data.scroll[1]
+					);
+
+					spr.color =
+						CoolUtil.colorFromString(
+							data.color
+						);
+
+					for (
+						varName in [
+							'alpha',
+							'angle'
+						]
+					)
 					{
-						var dat:Dynamic = Reflect.getProperty(data, varName);
-						if(dat != null) Reflect.setProperty(spr, varName, dat);
+						var dat:Dynamic =
+							Reflect.getProperty(
+								data,
+								varName
+							);
+
+						if (dat != null)
+						{
+							Reflect.setProperty(
+								spr,
+								varName,
+								dat
+							);
+						}
 					}
 
-					if (group != null) group.add(spr);
-					addedObjects.set(data.name, spr);
+					if (group != null)
+						group.add(spr);
+
+					addedObjects.set(
+						data.name,
+						spr
+					);
 
 				default:
-					var err = '[Stage .JSON file] Unknown sprite type detected: ${data.type}';
+					var err =
+						'[Stage .JSON file] Unknown sprite type detected: ${data.type}';
+
 					trace(err);
 					FlxG.log.error(err);
 			}
 		}
+
 		return addedObjects;
 	}
 
-	public static function validateVisibility(filters:LoadFilters)
+	public static function validateVisibility(
+		filters:LoadFilters
+	)
 	{
-		if((filters & STORY_MODE) == STORY_MODE)
-			if(!PlayState.isStoryMode) return false;
-		else if((filters & FREEPLAY) == FREEPLAY)
-			if(PlayState.isStoryMode) return false;
+		if (
+			(filters & STORY_MODE) == STORY_MODE
+		)
+		{
+			if (!PlayState.isStoryMode)
+				return false;
+		}
+		else if (
+			(filters & FREEPLAY) == FREEPLAY
+		)
+		{
+			if (PlayState.isStoryMode)
+				return false;
+		}
 
-		return ((ClientPrefs.data.lowQuality && (filters & LOW_QUALITY) == LOW_QUALITY) ||
-			(!ClientPrefs.data.lowQuality && (filters & HIGH_QUALITY) == HIGH_QUALITY));
+		return (
+			(
+				ClientPrefs.data.lowQuality
+				&& (
+					(filters & LOW_QUALITY)
+					== LOW_QUALITY
+				)
+			)
+			||
+			(
+				!ClientPrefs.data.lowQuality
+				&& (
+					(filters & HIGH_QUALITY)
+					== HIGH_QUALITY
+				)
+			)
+		);
 	}
 }
